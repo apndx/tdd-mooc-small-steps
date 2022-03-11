b20a3ff5c0dc287b0f9c2efdfd7be6e1366a80fe
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2022-03-11 14:09:14 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -45,7 +45,6 @@ function createApp(database) {
 
   function calculateReduction(date, date2) {
     let reduction = 0;
-    console.log(date, date2)
     if (date && date2 && isMonday(date, date2) && !isHoliday(date2)) {
       reduction = 35;
     }
