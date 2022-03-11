5177ce1b3204f12f62ac44a92f23438cc7ad97c0
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2022-03-11 12:38:53 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -60,8 +60,7 @@ function createApp(database) {
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
       let newHoliday = Temporal.PlainDate.from(row.holiday);
-      if ( date && date.getFullYear() === holiday.getFullYear() && date.getMonth() === holiday.getMonth() && date.getDate() === holiday.getDate() ) {
-        return true;
+      if ( date && date.getFullYear() === holiday.getFullYear() && date.getMonth() === holiday.getMonth() && date.getDate() === holiday.getDate() ) { return true;
       } else if (date2 && date2.year === newHoliday.year && date2.month === newHoliday.month && date2.day === newHoliday.day){return true;}
     }
     return false;
